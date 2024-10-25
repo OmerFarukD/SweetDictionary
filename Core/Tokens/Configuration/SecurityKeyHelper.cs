4463070b2e0340bccc3d67acf996be431648ae68
Pendik <Pendik@BIR>
2024-10-25 11:56:59 +0300

    puşladım

--- Core/Tokens/Configuration/SecurityKeyHelper.cs
+++ Core/Tokens/Configuration/SecurityKeyHelper.cs
@@ -7,6 +7,6 @@ public static class SecurityKeyHelper
 {
     public static SecurityKey GetSecurityKey(string securityKey)
     {
-        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey)); 
     }
 }
